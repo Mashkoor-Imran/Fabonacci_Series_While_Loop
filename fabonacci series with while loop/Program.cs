2b78350c8c4e6e156b355026deaf9e57ec3b5859
Mashkoor <mashkoorimran3@gmail.com>
2024-10-11 05:12:15 -0700

    intial commit

---
+++ fabonacci series with while loop/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Reflection.Metadata;
+
+Console.WriteLine("Enter The limit for Series");
+int limit = Convert.ToInt32(Console.ReadLine());
+if (limit > 0 && limit > 1)
+{
+
+    int first = 0;
+    Console.WriteLine(first);
+    int second = 1;
+    Console.WriteLine(second);
+    int third = 0;
+    while (third < limit)
+    {
+        third = first + second;
+        if (third <= limit)
+        {
+            Console.WriteLine(third);
+            first = second;
+            second = third;
+        }
+
+    }
+
+}
+else
+{
+    Console.WriteLine("Please Enter the Number Above 0 or 1");
+
+}
+
+
